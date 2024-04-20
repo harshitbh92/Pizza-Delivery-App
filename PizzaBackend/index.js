3c247bd33073cb3e4388d5f6148fa1ff0ed3bdd1
lion656544 <100365269+lion656544@users.noreply.github.com>
2024-04-20 14:51:56 +0530

    Db connection, Error Handling

--- PizzaBackend/index.js
+++ PizzaBackend/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app =express();
+const dbConnect = require('./config/dbConnect')
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
 const cors = require('cors');
+
+dbConnect();
 
 app.listen(PORT,()=>{
     console.log(`Server listening on PORT ${PORT}`);
